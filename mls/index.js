a3db03099a27e852f52a574b54ba7ff01907e568
Kevin Solorio <soloriok@gmail.com>
2019-03-06 21:56:24 -0800

    extract taxes and association fees from listing

--- mls/index.js
+++ mls/index.js
@@ -1,4 +1,6 @@
 (function() {
+  // functions are coupled to the DOM of the @properties MLS
+  // changes to the DOM will break these functions
   function getAddress() {
     const addressNode = document.getElementsByClassName("pl-head-address")[0];
 
@@ -9,7 +11,7 @@
   }
 
   function getPrice() {
-    const priceRegexp = /[\$,]/g
+    const priceRegexp = /[\$,]/g;
     const priceNode = document.getElementsByClassName("rc-price")[0];
 
     if (priceNode) {
@@ -18,10 +20,50 @@
     return 0;
   }
 
+  function getAssociationFees() {
+    const labelText = "Assess / Assoc Fee:"
+    const tableRow = getRowWithText(labelText);
+
+    return dollarAmountFromText(tableRow.textContent)
+  }
+
+  function getTaxes() {
+    const labelText = "Total Taxes";
+    const tableRow = getRowWithText(labelText);
+
+    if (tableRow) {
+      return dollarAmountFromText(tableRow.textContent)
+    }
+    return 0;
+  }
+
+  function getRowWithText(text) {
+    const tableIndex = 1;
+    const mainInfoClass = "detail-main-info-section";
+    const table = document.getElementsByClassName(mainInfoClass)[tableIndex];
+
+    const tableRows = Array.from(table.getElementsByTagName("tr"));
+    return tableRows.find(row => row.textContent.match(text));
+  }
+
+  function dollarAmountFromText(text) {
+    const priceRegexp = /\$(\d{1,3}[,\d{1,3}]{0,}\.?\d{0,2})/
+    const matches = text.match(priceRegexp);
+
+    if (matches) {
+      return parseFloat(matches[1].replace(/,/g, ''));
+    }
+    return 0;
+  }
+
   const listing = {
     address: getAddress(),
-    price: getPrice()
+    price: getPrice(),
+    associationFees: getAssociationFees(),
+    taxes: getTaxes()
   }
+
+  console.log(listing);
 
   chrome.storage.local.set({listing});
 })();
